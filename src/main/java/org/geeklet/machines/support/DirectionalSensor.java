0ff8d3bcd65a0dd916bc93d7750cfc87e3945d42
Paul Willey <paul.willey@workday.com>
2016-12-01 16:19:07 -0800

    Framework for the seonsors

---
+++ src/main/java/org/geeklet/machines/support/DirectionalSensor.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package org.geeklet.machines.support;
+
+/**
+
+ */
+public abstract class DirectionalSensor implements iSensor {
+	int
+}
---
+++ src/main/java/org/geeklet/machines/support/iSensor.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package org.geeklet.machines.support;
+
+/**
+ *
+ */
+public interface iSensor {
+	boolean triggered ();
+}
