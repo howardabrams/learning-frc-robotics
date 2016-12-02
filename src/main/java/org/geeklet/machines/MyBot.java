bd836b4e1879474684f949b3e6e28abfa7e283a1
Howard Abrams <howard.abrams@workday.com>
2016-12-02 10:32:45 -0800

    Code for 20-learning-java
    Updated the comments and the code for what should be in the
    20-learning-java.md document. Eventually, we will want to remove this.

--- src/main/java/org/geeklet/machines/MyBot.java
+++ src/main/java/org/geeklet/machines/MyBot.java
@@ -1,30 +1,48 @@
-/**
- * 
+/*
+ * The example robot that will accompany the lesson chapter:
+ *     docs/20-learning-java
  */
 package org.geeklet.machines;
 
 import org.geeklet.machines.support.Robot;
 
 /**
- *
+ * The most simplest robot that still functions in our virtual world.
+ * Create the coordinate variables, <code>x</code> and <code>y</code>,
+ * as well as the <code>direction</code> (which is in degrees).
+ * <p>
+ * The behavior simply moves the robot along the X axis.
  */
 public class MyBot extends Robot {
-	public int x;
-	public int y;
-	public int direction;
-	
-	/**
-	 * 
-	 */
-	public MyBot(int x, int y, int direction) {
-		this.x = x;
-		this.y = y;
-		this.direction = direction;
-	}
+    /** The left-right coordinate for the Robot's placement on the field. */
+    public int x;
+    /** The up-down coordinate for the Robot's placement on the field. */
+    public int y;
+    /** The angle in degrees of the Robot's direction. */
+    public int direction;
 
-	@Override
-	public void step() {
-		x += 2;
-	}
+    /**
+     * The typical constructor that takes the field coordinate
+     * placement as well as the initial pointing direction.
+     *
+     * @param x   The desired placement coordinate on the field
+     * @param y   Desired placement coordinate on the field
+     * @param direction Initial requested direction for the robot,
+     *            where <code>0</code> degrees points to the right.
+     */
+    public MyBot(int x, int y, int direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
 
+    /**
+     * The <i>robotic world</i> will repeatedly call this function for
+     * the robot to alter its behavior. In this case, we simply
+     * increment the <code>x</code> variable.
+     */
+    @Override
+    public void step() {
+        x += 2;
+    }
 }
--- src/main/java/org/geeklet/machines/MyGame.java
+++ src/main/java/org/geeklet/machines/MyGame.java
@@ -1,5 +1,6 @@
-/**
- * 
+/*
+ * Example `game` that accompanies the lesson chapter:
+ *     docs/20-learning-java.md
  */
 package org.geeklet.machines;
 
@@ -8,19 +9,25 @@ import org.geeklet.machines.support.FootballField;
 import org.geeklet.machines.support.Robot;
 
 /**
- *
+ * The simplest game that adds the {@link MyBot} robot to the
+ * {@link FootballField} field.
  */
 public class MyGame {
+    /**
+     * The interface to the <b>Run...</b> menu item in Eclipse that
+     * does the work of connecting the robot to the field, and then
+     * calls the {@link Field#runGame()} method to start the fun.
+     * @param args These are ignored
+     */
+    public static void main(String[] args) {
+        // The field takes a string that is used for the Window title.
+        Field field = new FootballField("My Fun Game");
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Robot myKit = new SimpleRobot(42, 100, 0);
-		Field field = new FootballField("My Fun Game");
-		
-		field.addRobot(myKit);
-		field.runGame();
-	}
+        // Create and place the robot on the "left" side of the field
+        Robot myKit = new MyBot(10, 200, 0);
 
+        field.addRobot(myKit);
+
+        field.runGame();
+    }
 }
